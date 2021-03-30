bde8c9f171a5c13b5e1816acb66afb2d9d8b92e5
MOCHAMMAD ADAM'S ARZAQI <1841720075@student.polinema.ac.id>
2021-03-30 11:51:24 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+import './index.css';
+import App from './App';
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+const store = createStore(rootReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+reportWebVitals();
